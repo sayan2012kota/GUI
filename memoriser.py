4083674ef3307413a6c89aa43f87ad45d29de11c
sayan2012kota <sayankota2012@gmail.com>
2026-02-22 09:56:36 +0000

    Add files via upload

--- memoriser.py
+++ memoriser.py
@@ -1,11 +1,24 @@
 from tkinter import *
+from tkinter.filedialog import *
 window = Tk()
 window.geometry("700x450")
-button =  Button(window, text = "SAVE")
+def delete_file():
+    selected_file = listbox.curselection()
+    listbox.delete(selected_file)
+def add_file():
+    listbox.insert(END, entry.get())
+    entry.delete(0, END)
+def save_file():
+    saved_file = asksaveasfile(defaultextension=".txt")
+
+#def open_file():
+
+
+button =  Button(window, text = "SAVE", command = save_file)
 button.place(x=325, y=5)
 entry = Entry(window)
 entry.place(x=275, y=40)
-button2 = Button(window, text = "ADD")
+button2 = Button(window, text = "ADD", command = add_file)
 button2.place(x=326, y=65)
 scrollbar = Scrollbar(window)
 scrollbar.pack(side = RIGHT, fill=Y)
@@ -13,11 +26,13 @@ listbox = Listbox(window, height = 15, width=50, yscrollcommand=scrollbar.set, b
 scrollbar.config(command=listbox.yview)
 listbox.place(x=175, y=150)
 for i in range(30):
-    listbox.insert(END, "List", i)
-button3 = Button(window, text = "OPEN", width = 15)
+    listbox.insert(END, "List" +  str(i))
+button3 = Button(window, text = "OPEN", width = 15, )
 button3.place(x=45, y=180)
-button4 = Button(window, text = "DELETE", width = 15)
+button4 = Button(window, text = "DELETE", width = 15, command = delete_file)
 button4.place(x=550, y=180)
+
+
 
 
 
