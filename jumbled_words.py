3709d6a345571afcef7fe889e0e8a1905ed92528
sayan2012kota <sayankota2012@gmail.com>
2026-05-03 09:09:29 +0100

    Add files via upload

--- jumbled_words.py
+++ jumbled_words.py
@@ -3,6 +3,7 @@ import random
 from tkinter import messagebox
 window = Tk()
 window.geometry("400x500")
+window.configure(background="green")
 word_list = ["satisfied", "scrape","seize","policeman","buttocks","trouble","weave","scream","load","clarify"]
 word_choice = random.choice(word_list)
 word = list(word_choice)
@@ -10,6 +11,7 @@ random.shuffle(word)
 shuffled_word = "".join(word)
 print(shuffled_word)
 score = 0
+lives = 3
 def choose_new_word():
     global word_choice
     word_choice = random.choice(word_list)
@@ -20,15 +22,18 @@ def choose_new_word():
     
 def check_answer():
     global score
+    global lives
     answer = entry.get()
     if answer == word_choice:
         score = score + 1
         messagebox.showinfo("Result", "Your answer was right")
     else:
         messagebox.showinfo("Result", "Your answer was wrong")
+        lives = lives - 1
     print(score)
     choose_new_word()
-    label3.config(text = str(score))
+    label3.config(text = "Score:" + str(score))
+    label4.config(text = "Lives left:" + str(lives))
     entry.delete(0, END)
 def reset():
     global score
@@ -49,8 +54,10 @@ button = Button(window, text = "Check", command = check_answer)
 button.place(x=150, y = 300)
 button2 = Button(window, text = "Reset", command = reset)
 button2.place(x=150, y=350)
-label3 = Label(window, text = score)
+label3 = Label(window, text = "Score:" + str(score))
 label3.place(x = 10, y=475)
+label4 = Label(window, text  = "Lives left:" + str(lives))
+label4.place(x=10, y=450)
 
 
 
